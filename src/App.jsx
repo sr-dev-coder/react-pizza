87d44185f1b2d7819a00eed6b5df695ab6345e8c
sandeepr <sandeeprohillainfo@gmail.com>
2025-06-02 11:02:30 +0530

    Added redux in app

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,9 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+      <RouterProvider router={router} />
+  )
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ReduxProvider } from "./redux/provider"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>
 );
